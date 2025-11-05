08821b91f47e9117a38c277e7e19f80d871f3af3
throusea <1353272517@qq.com>
2025-11-05 14:23:49 +0800

    Plot the risk in quick_run.py

--- quick_run.py
+++ quick_run.py
@@ -1,8 +1,22 @@
+import numpy as np
 from record import Record
 def main():
 
     # Record(1).draw_frame_risk(2) # draw one frame
-    Record(1).calculate_risk(1,2) # test one frame,test the risk of one vehicle in one frame
+    # Record(1).calculate_risk(1,2) # test one frame,test the risk of one vehicle in one frame
+    X = np.arange(1, 15)
+    Y = []
+    for i in range(1,15):
+        _, risk, _ = Record(1).calculate_risk(i,2) # test one frame,test the risk of one vehicle in one frame
+        Y.append(risk)
+    Y = np.array(Y)
+    import matplotlib.pyplot as plt
+    plt.plot(X, Y)
+    plt.xlabel('Vehicle ID')
+    plt.ylabel('Risk')
+    plt.title('Risk vs Vehicle ID')
+    plt.grid()
+    plt.show()
 
 if __name__ == "__main__":
     main()
